bf0bba91e5da365b0426575aab0f21ec53be1d31
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-08-05 16:54:20 -0300

    Getting up and running: SKCropNode

---
+++ Project 14/WhackSlot.swift
@@ -0,0 +1,32 @@
+//
+//  WhackSlot.swift
+//  Project 14
+//
+//  Created by Henrique Silva on 05/08/21.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zRotation = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+}
