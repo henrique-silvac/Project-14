d9d42e3d733edf0859bd0ed5ea1174a8ce6da4f8
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-08-06 14:44:37 -0300

    Wrap Up

--- Project 14/GameScene.swift
+++ Project 14/GameScene.swift
@@ -8,16 +8,23 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
+    
+    var popupTime = 0.85
+    var numRounds = 0
+    
+    var hitFriendSound: SKAction!
+    var hitEnemySound: SKAction!
+    var gameOverSound1: SKAction!
+    var gameOverSound2: SKAction!
+    
     var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
         }
     }
-    
-    var slots = [WhackSlot]()
-    var popupTime = 0.85
-    var numRounds = 0
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -33,47 +40,68 @@ class GameScene: SKScene {
         gameScore.fontSize = 48
         addChild(gameScore)
         
-        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
-        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
-        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
-        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [unowned self] in
-            self.createEnemy()
+        hitFriendSound = SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false)
+        hitEnemySound = SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false)
+        gameOverSound1 = SKAction.playSoundFileNamed("43698__notchfilter__game-over03.caf", waitForCompletion: true)
+        gameOverSound2 = SKAction.playSoundFileNamed("72866__tim-kahn__game-over.caf", waitForCompletion: false)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let location = touch.location(in: self)
-            let tappedNodes = nodes(at: location)
-            
-            for node in tappedNodes {
-                if node.name == "charFriend" {
-                    // they shouldn't have whacked this penguin
-                    let whackSlot = node.parent!.parent as! WhackSlot
-                    if !whackSlot.isVisible { continue }
-                    if whackSlot.isHit { continue }
-                    
-                    whackSlot.hit()
-                    score -= 5
-                    
-                    run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion:false))
-                } else if node.name == "charEnemy" {
-                    // they should have whacked this one
-                    let whackSlot = node.parent!.parent as! WhackSlot
-                    if !whackSlot.isVisible { continue }
-                    if whackSlot.isHit { continue }
-                    
-                    whackSlot.charNode.xScale = 0.85
-                    whackSlot.charNode.yScale = 0.85
-                    
-                    whackSlot.hit()
-                    score += 1
-                    
-                    run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion:false))
-                }
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes {
+            if node.name == "newGame" {
+                startNewGame()
+                return
             }
+            
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                // they shouldn't have whacked this penguin
+                score -= 5
+                
+                run(hitFriendSound)
+            }
+            else if node.name == "charEnemy" {
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+                score += 1
+                
+                run(hitEnemySound)
+            }
+        }
+    }
+
+    func startNewGame() {
+        for child in children {
+            if child.name == "gameOver" || child.name == "finalScore" || child.name == "newGame"{
+                removeChildren(in: [child])
+            }
+        }
+        
+        popupTime = 0.85
+        numRounds = 0
+        score = 0
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
         }
     }
     
@@ -83,7 +111,7 @@ class GameScene: SKScene {
         addChild(slot)
         slots.append(slot)
     }
-    
+
     func createEnemy() {
         numRounds += 1
         
@@ -95,8 +123,30 @@ class GameScene: SKScene {
             let gameOver = SKSpriteNode(imageNamed: "gameOver")
             gameOver.position = CGPoint(x: 512, y: 384)
             gameOver.zPosition = 1
+            gameOver.name = "gameOver"
             addChild(gameOver)
             
+            let finalScore = SKLabelNode(fontNamed: "Chalkduster")
+            finalScore.text = "Final score: \(score)"
+            finalScore.zPosition = 1
+            finalScore.position = CGPoint(x: 512, y: 314)
+            finalScore.horizontalAlignmentMode = .center
+            finalScore.fontSize = 32
+            finalScore.name = "finalScore"
+            addChild(finalScore)
+
+            let gameOverSoundSequence = SKAction.sequence([gameOverSound1, gameOverSound2])
+            run(gameOverSoundSequence)
+
+            let newGame = SKLabelNode(fontNamed: "Verdana Bold")
+            newGame.text = "> NEW GAME <"
+            newGame.zPosition = 1
+            newGame.position = CGPoint(x: 512, y: 264)
+            newGame.horizontalAlignmentMode = .center
+            newGame.fontSize = 24
+            newGame.name = "newGame"
+            addChild(newGame)
+
             return
         }
         
@@ -106,16 +156,16 @@ class GameScene: SKScene {
         slots[0].show(hideTime: popupTime)
         
         if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
-        if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
-        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime) }
         
         let minDelay = popupTime / 2.0
         let maxDelay = popupTime * 2
         let delay = Double.random(in: minDelay...maxDelay)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [unowned self] in
-            self.createEnemy()
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
         }
     }
 }
--- Project 14/WhackSlot.swift
+++ Project 14/WhackSlot.swift
@@ -35,40 +35,104 @@ class WhackSlot: SKNode {
     
     func show(hideTime: Double) {
         if isVisible { return }
+
+        mudEffect(direction: .up)
         
         charNode.xScale = 1
         charNode.yScale = 1
+
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
-        
         isVisible = true
         isHit = false
         
         if Int.random(in: 0...2) == 0 {
             charNode.texture = SKTexture(imageNamed: "penguinGood")
             charNode.name = "charFriend"
-        } else {
+        }
+        else {
             charNode.texture = SKTexture(imageNamed: "penguinEvil")
             charNode.name = "charEnemy"
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [unowned self] in
-            self.hide()
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
         }
     }
     
     func hide() {
         if !isVisible { return }
         
-        charNode.run(SKAction.moveBy(x: 0, y:-80, duration:0.05))
+        mudEffect(direction: .down)
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
     }
     
     func hit() {
         isHit = true
+
+        smokeEffect()
         
         let delay = SKAction.wait(forDuration: 0.25)
-        let hide = SKAction.moveBy(x: 0, y:-80, duration:0.5)
-        let notVisible = SKAction.run { [unowned self] in self.isVisible = false }
-        charNode.run(SKAction.sequence([delay, hide, notVisible]))
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [weak self] in
+            self?.isVisible = false
+        }
+        
+        let sequence = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequence)
+    }
+
+    func smokeEffect() {
+        if let smoke = SKEmitterNode(fileNamed: "SmokeEmitter") {
+            smoke.position = CGPoint(x: 0, y: 45)
+            smoke.zPosition = 1
+            smoke.numParticlesToEmit = 10
+            smoke.particleLifetime = 0.75
+            smoke.particleColor = SKColor.white
+            smoke.particleAlpha = 0.2
+
+            effectSequence(effect: smoke)
+        }
+    }
+    
+    enum Direction {
+        case up
+        case down
+    }
+    
+    func mudEffect(direction: Direction) {
+        if let mud = SKEmitterNode(fileNamed: "MudEmitter") {
+            mud.position = CGPoint(x: 0, y: 0)
+            mud.zPosition = 1
+            mud.numParticlesToEmit = 100
+            mud.particleBirthRate = 500
+            mud.particleSize = CGSize(width: 30, height: 30)
+            mud.particleColor = SKColor.brown
+            mud.particleBlendMode = .replace
+            switch direction {
+            case .up:
+                mud.particleLifetime = 0.30
+                mud.particleSpeed = 1
+                mud.particleSpeedRange = 300
+            case .down:
+                mud.particleLifetime = 0.10
+                mud.particleSpeed = 100
+                mud.particleSpeedRange = 0
+            }
+            
+            effectSequence(effect: mud)
+        }
+    }
+
+    func effectSequence(effect: SKEmitterNode) {
+        let action = SKAction.run { [weak self] in
+            self?.addChild(effect)
+        }
+        let duration = SKAction.wait(forDuration: 2)
+        let removal = SKAction.run { [weak self] in
+            self?.removeChildren(in: [effect])
+        }
+        run(SKAction.sequence([action, duration, removal]))
     }
 }
